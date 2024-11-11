36adc005c234ca225dbd8f842c5587aa8e719d38
RosinB <karta1499002@gmail.com>
2024-11-11 16:20:07 +0800

    s

--- tickets-backend/src/main/java/com/example/demo/controller/EventController.java
+++ tickets-backend/src/main/java/com/example/demo/controller/EventController.java
@@ -1,24 +1,32 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.dto.event.EventDto;
+import com.example.demo.service.event.EventService;
 import com.example.demo.util.ApiResponse;
 
 @RestController
 @Controller
 public class EventController {
 
+	@Autowired
+	EventService eventService;
+	
 	@GetMapping 
 	ResponseEntity<ApiResponse<Object>> getEvent(){
 		
+		List<EventDto> eventDto=eventService.findAllEvent();
 		
 		
 		
-		
-		return null;
+		return ResponseEntity.ok(ApiResponse.success("查詢所有活動成功", eventDto));
 	}
 	
 	
